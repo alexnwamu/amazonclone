e0bd3488d6af5bb29f0685d0dfd4a119736e3b7a
Dinaka Nwamu <dinakanwamu@gmail.com>
2023-03-27 11:46:53 +0100

    Amazon Image

--- src/components/Header.js
+++ src/components/Header.js
@@ -20,7 +20,7 @@ const Header = () => {
       <div className='flex items-center bg-amazon_blue p-1 flex-grow py-2 '>  
       <div className='mt-2 flex items-center flex-grow sm:flex-grow-0 ' >
         <Image onClick={( ) => router.push
-        ('/')} src='/../public/amazon_PNG11.png'
+        ('/')} src='/amazon_PNG11.png'
         width={150}
         height ={40}
         objectFit = 'contain'
