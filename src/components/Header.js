f4cc0df51dddf76cf3679db31e4ef08cbb5b8c75
Dinaka Nwamu <dinakanwamu@gmail.com>
2023-03-27 11:37:29 +0100

    Amazon Image

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Image from 'next/image';
+import Image from 'next/dist/client/image' 
 import {useRouter} from 'next/router'
 import { signIn, signOut, useSession } from 'next-auth/react'
 import {
@@ -18,13 +18,14 @@ const Header = () => {
     <header>
       {/* Top Navbar */}
       <div className='flex items-center bg-amazon_blue p-1 flex-grow py-2 '>  
-      <div className='mt-2 flex items-center flex-grow sm:flex-grow-0' >
+      <div className='mt-2 flex items-center flex-grow sm:flex-grow-0 ' >
         <Image onClick={( ) => router.push
-        ('/')} src='https://links.papareact.com/f90'
+        ('/')} src='/../public/amazon_PNG11.png'
         width={150}
         height ={40}
         objectFit = 'contain'
-        className='cursor-pointer'
+
+        className='cursor-pointer p-2'
         />
       </div>
       {/* Searchbar */}
